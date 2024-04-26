15913dcf51e0a2b5ce4c606d207cca1128e5ab1e
DrCetmi <drcetmi@gmail.com>
2024-04-26 22:51:15 +0200

    deployed backend1

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const port = process.env.PORT || 8001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "https://cloudinary-integration.onrender.com" }));
+app.use(cors({ origin: "https://cloudinary-integration.vercel.app" }));
 
 app.get("/users", async (req, res) => {
   try {
